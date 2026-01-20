3afec374a8ea1c729d63c47b7d177f36438ce87f
nanangarit96 <nanangarit96@gmail.com>
2026-01-20 14:50:32 +0700

    Configure base path in vite.config.ts
    Set base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Ubah base sesuai repo kamu
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
   },
+  base: '/LUXURY-GIORGIO-ARMANI/', // Penting agar asset path benar di GitHub Pages
 });
